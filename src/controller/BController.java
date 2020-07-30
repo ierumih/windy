70d1e86072a4d5238f835c6c422c8b8d89108d42
Administrator <Administrator@321-20>
2020-07-30 16:12:54 +0900

    됨?

---
+++ src/controller/BController.java
@@ -0,0 +1,112 @@
+package com.windy.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.windy.board.action.*;
+import com.windy.vo.ActionForward;
+
+@WebServlet("*.b")
+public class BController extends javax.servlet.http.HttpServlet{
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		
+		request.setCharacterEncoding("UTF-8");
+		String RequestURI=request.getRequestURI();
+		String contextPath=request.getContextPath();
+		String command=RequestURI.substring(contextPath.length());
+		ActionForward forward=null;
+		Action action=null;
+
+		if(command.equals("/write.b")){
+			forward=new ActionForward();
+			forward.setPath("/write.jsp");
+		}
+		else if(command.equals("/writeaction.b")){
+			action = new BoardWriteAction();
+			try{
+				forward=action.execute(request, response);
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+		else if(command.equals("/editaction.b")){
+			action = new BoardEditAction();
+			try{
+				forward=action.execute(request, response);
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+		else if(command.equals("/deleteaction.b")){
+			action = new BoardDeleteAction();
+			try{
+				forward=action.execute(request, response);
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+		else if(command.equals("/loginaction.b")){
+			action = new LoginAction();
+			try{
+				forward=action.execute(request, response);
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+		else if(command.equals("/joinaction.b")){
+
+			action = new JoinAction();
+			try{
+				forward=action.execute(request, response);
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+		else if(command.equals("/boardlist.b")){
+			action = new BoardListAction();
+			try{
+				forward=action.execute(request, response);
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+		else if(command.equals("/boardview.b")){
+			action = new BoardViewAction();
+			try{
+				forward=action.execute(request, response);
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+
+			
+		if(forward != null){
+			
+			if(forward.isRedirect()){
+				response.sendRedirect(forward.getPath());
+			}else{
+				RequestDispatcher dispatcher=
+						request.getRequestDispatcher(forward.getPath());
+				dispatcher.forward(request, response);
+			}
+			
+		}
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		doProcess(request,response);
+	}  	
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		doProcess(request,response);
+	}   
+}
+
